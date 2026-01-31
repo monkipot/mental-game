ab1aa7de69209afec8bf00c3e1737c06120b00cd
monki <trad@trackstone.fr>
2026-01-31 22:20:42 +0100

    feat(game): display difficulty buttons

--- src/jeux_des_lettres.ts
+++ src/jeux_des_lettres.ts
@@ -1,1 +1,23 @@
 console.log('jeux des lettres');
+
+enum LEVEL {
+    EASY = 'Simple',
+    MIDDLE = 'Moyen',
+    HARD = 'Difficile',
+    EXPERT = 'EXPERT'
+}
+
+const getLevels = () => Object.values(LEVEL);
+
+const renderLevels = () => {
+    const levelsContainer = document.getElementById('levels');
+    if (!levelsContainer) return;
+
+    getLevels().forEach(level => {
+        const button = document.createElement('button');
+        button.textContent = level;
+        levelsContainer.appendChild(button);
+    });
+}
+
+renderLevels();
