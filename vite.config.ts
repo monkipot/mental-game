3e1209ed93905b2216a55de363214f0f11145392
monki <trad@trackstone.fr>
2026-02-01 16:48:32 +0100

    fix: add brain

--- vite.config.ts
+++ vite.config.ts
@@ -2,4 +2,13 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
     base: "/mental-game/",
+    build: {
+        rollupOptions: {
+            input: {
+                main: './index.html',
+                'jeu-calcul-mental': './jeu_calcul_mental.html',
+                'jeu-des-lettres': './jeu_des_lettres.html',
+            }
+        }
+    }
 });
